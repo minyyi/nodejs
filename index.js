5efa2ee557de812d26ed561708e75a72c637afc9
MYAIR <ymy0613@gmail.com>
2024-06-27 21:08:39 +0900

    추가: https 코드 추가

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const Product = require("./model/productModel.js");
+const https = require("https");
+const fs = require("fs");
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync(__dirname + "/key.pem", "utf-8"),
+      cert: fs.readFileSync(__dirname + "/cert.pem", "utf-8"),
+    },
+    function (req, res) {
+      res.write("Congrats! You made https server now :)");
+      res.end();
+    }
+  )
+  .listen(5502);
 
 mongoose
   .connect(
